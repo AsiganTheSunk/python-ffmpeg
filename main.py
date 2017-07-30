52dc5ef597a59ff5bb6128ab24355e7da36165c9
Pedro Arias Ruiz <asiganthesunk@gmail.com>
2017-07-31 00:19:48 +0200

    Minor Changes
    added some usefull functions

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 from main.FFmpegClient import FFmpegClient
+import main.exiftool as ExifTool
+import os
 
 def main():
+    metadata_path = str(os.getcwd() + '/test/OutputMetadata.txt')
+    source = str(os.getcwd()) + '/test/SampleVideoSubtitles.mkv'
 
-    ffmpeg = FFmpegClient()
+    #ffmpeg = FFmpegClient()
     #ffmpeg.extract_subtitles()
     #ffmpeg.extract_audio()
-    ffmpeg.extract_metadata()
+    ExifTool.extract_metadata(path=source, dest_path=metadata_path)
+    ExifTool.retrieve_audio_codec(path=metadata_path)
+    ExifTool.retrieve_video_codec(path=metadata_path)
+    ExifTool.retrieve_track_name(path=metadata_path)
+    ExifTool.retrieve_track_language(path=metadata_path)
+    print '----'*10
+    ExifTool.retrieve_audio_info(path=metadata_path)
+    print '----' * 10
+    ExifTool.retrieve_video_info(path=metadata_path)
+    print '----' * 10
+    ExifTool.retrieve_file_info(path=metadata_path)
+
     return
 
 
--- main/exiftool.py
+++ main/exiftool.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 import subprocess, os
+import re
+
+AUDIO_CODEC = 'Audio Codec ID'
+VIDEO_CODEC = 'Video Codec ID'
+TRACK_NUMBER = 'Track Number'
+TRACK_NAME = 'Track Name'
+TRACK_LANGUAGE = 'Track Language'
 
 def extract_metadata(path='', dest_path=''):
-    path = str(os.getcwd()) + '/test/SampleVideoSubtitles.mkv'
-    dest_path = str(os.getcwd() + '/test/OutputMetadata.txt')
+
     try:
         print ('Exiftool: Retrieving Metadata from - ' + path)
         COMMAND = 'exiftool ' + path + ' >' + dest_path
@@ -15,20 +21,73 @@ def extract_metadata(path='', dest_path=''):
         print('Exiftool: Something went wrong retrieving Metadata from - ' + path)
         return
 
-def inyect_metadata(self, path='', dest_path=''):
+def inyect_metadata(path='', dest_path=''):
     return
 
 
-
-def extract_audio_info(self):
-    return
-
-def extract_video_codec(self):
+def extract_audio_info():
     return
 
 
-def extract_audio_metadata(self):
-    self.extract_video_codec()
-    self.extract_video_res()
-    self.extract_video_name()
+def extract_video_codec():
+    return
+
+
+def retrieve_file_info(path='', flag=''):
+    if flag is '':
+        try:
+            with open(path) as f:
+                lines = f.readlines()
+                for line in lines:
+                    print line[:-1]
+        except:
+            print('Exiftool: Something went wrong retrieving ' + flag + ' from - ' + path)
+            return
+
+    else:
+        try:
+            with open(path) as f:
+                lines = f.readlines()
+                for line in lines:
+                    if flag in line:
+                        print(str('Exiftool: ' + flag + ' - ' + line[34:-1]))
+                        return
+        except Exception as e:
+            print('Exiftool: Something went wrong retrieving ' + flag + ' from - ' + path)
+            return
+
+
+def retrieve_audio_codec(path=''):
+    return retrieve_file_info(path=path, flag=AUDIO_CODEC)
+
+
+def retrieve_video_codec(path=''):
+    return retrieve_file_info(path=path, flag=VIDEO_CODEC)
+
+
+def retrieve_track_name(path=''):
+    return retrieve_file_info(path=path, flag=TRACK_NAME)
+
+
+def retrieve_track_language(path=''):
+    return retrieve_file_info(path=path, flag=TRACK_LANGUAGE)
+
+
+def retrieve_track_number(path=''):
+    return retrieve_file_info(path=path, flag=TRACK_NUMBER)
+
+
+def retrieve_video_info(path=''):
+    retrieve_video_codec(path=path)
+
+
+def retrieve_audio_info(path=''):
+    retrieve_audio_codec(path=path)
+    retrieve_track_name(path=path)
+
+
+
+def retrieve_subtitles_info(path=''):
+    retrieve_track_number(path=path)
+
 
